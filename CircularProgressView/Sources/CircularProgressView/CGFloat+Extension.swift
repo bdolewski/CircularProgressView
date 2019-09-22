d30d887211eb735510cd1c7624abec7ba6cacdec
Bartosz Dolewski <ext.bartosz.a.dolewski@tieto.com>
2019-09-22 17:35:55 +0200

    Converted to Swift Package Manager

---
+++ CircularProgressView/Sources/CircularProgressView/CGFloat+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  CGFloat+Extension.swift
+//  CircularProgressView
+//
+//  Created by Bartosz Dolewski on 23/04/2019.
+//  Copyright © 2019 Bartosz Dolewski. All rights reserved.
+//
+
+import UIKit
+
+internal extension CGFloat {
+    var half: CGFloat {
+        return self / 2
+    }
+}
